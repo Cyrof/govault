7382f3eb94b36b8b3c4b3d19b26819e34faf5438
keith <keithneo00@gmail.com>
2025-07-27 01:06:53 +0800

    refactor(vault/): Fixed some issues with the vault not working properly and also added new implementation of crypto and file io functions

--- internal/vault/vault.go
+++ internal/vault/vault.go
@@ -3,10 +3,11 @@ package vault
 import (
 	"github.com/Cyrof/govault/internal/crypto"
 	"github.com/Cyrof/govault/internal/fileIO"
+	"github.com/Cyrof/govault/internal/model"
 )
 
 type Vault struct {
-	Secrets map[string]string
+	Secrets map[string]model.Secret
 	FileIO  *fileIO.FileIO
 	Crypto  *crypto.Crypto
 }
@@ -14,7 +15,7 @@ type Vault struct {
 // constructor function
 func NewVault(fileIO *fileIO.FileIO, crypto *crypto.Crypto) *Vault {
 	return &Vault{
-		Secrets: make(map[string]string),
+		Secrets: make(map[string]model.Secret),
 		FileIO:  fileIO,
 		Crypto:  crypto,
 	}
