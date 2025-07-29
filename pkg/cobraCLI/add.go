cb6989b43d68443b8b22a974e9fc733fbb159e1c
keith <keithneo00@gmail.com>
2025-07-29 17:52:58 +0800

    feat(get, vars): Added the get command cli using cobra. Also added a package level variables
    refactor(add): Added more description to the command for help command

--- pkg/cobraCLI/add.go
+++ pkg/cobraCLI/add.go
@@ -6,14 +6,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	key   string
-	value string
-)
-
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a new secret",
+	Use:     "add",
+	Aliases: []string{"a"},
+	Short:   "Add a new secret",
+	Long:    `The 'add' command allows you to securely store a key-value pair in your local encrypted vault.`,
+	Example: `	
+		govault add --key github --value myGitHubToken123
+		govault a -k email -v mypassword
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		v.AddSecret(key, value)
 		fmt.Println("Secret added.")
