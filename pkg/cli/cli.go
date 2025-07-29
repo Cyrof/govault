5ac3d1bb88c6f34ac959e1fc21a9d234efd585af
keith <keithneo00@gmail.com>
2025-07-29 18:17:58 +0800

    chore(cli, purge): Removed all unused codes and files

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,7 +1,6 @@
 package cli
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -50,86 +49,4 @@ func Setup(v *vault.Vault) {
 		time.Sleep(1 * time.Second)
 		ClearScreen()
 	}
-}
-
-func Execute(v *vault.Vault) {
-	// main commands
-	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
-	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
-	purgeCmd := flag.NewFlagSet("purge", flag.ExitOnError)
-	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
-
-	// sub args for add arg
-	addKey := addCmd.String("key", "", "Key to store")
-	addVal := addCmd.String("value", "", "Value to store")
-
-	// sub args for get arg
-	getKey := getCmd.String("key", "", "Key to retrieve")
-
-	if len(os.Args) < 2 {
-		PrintUsage()
-		os.Exit(1)
-	}
-
-	switch os.Args[1] {
-	case "add":
-		Setup(v)
-		addCmd.Parse(os.Args[2:])
-		// add secret to vault
-		v.AddSecret(*addKey, *addVal)
-		fmt.Println("Secret added.")
-		v.Save()
-	case "get":
-		Setup(v)
-		getCmd.Parse(os.Args[2:])
-		val, ok := v.GetSecret(*getKey)
-		if ok {
-			fmt.Println("Value:", val.Value)
-		} else {
-			fmt.Println("Error retrieving secret")
-		}
-	case "purge":
-		purgeCmd.Parse(os.Args[2:])
-		confirm := PromptPurge()
-		if confirm {
-			v.FileIO.PurgeVault()
-			fmt.Println("All vault data has been successfully purged. The system has been reset.")
-		} else {
-			fmt.Println("Purge operation cancelled. No changes were made.")
-			os.Exit(1)
-		}
-	case "list":
-		Setup(v)
-		listCmd.Parse(os.Args[2:])
-		v.DisplayKeys()
-		os.Exit(1)
-	default:
-		PrintUsage()
-		os.Exit(1)
-	}
-}
-
-func PrintUsage() {
-	fmt.Println("Usage:")
-	// add key description
-	fmt.Println("	govault add -key <key> -value <value>")
-	fmt.Println("		Add a new secret to the vault.")
-
-	// get key description
-	fmt.Println("	\n\tgovault get -key <key>")
-	fmt.Println("		Retrieve a stored secreted from the vault by its key.")
-
-	// list all keys description
-	fmt.Println("	\n\tgovault list")
-	fmt.Println("		List all keys stored in value.")
-
-	// purge vault description
-	fmt.Println("	\n\tgovault purge")
-	fmt.Println("		Permanently delete all stored vault data and reset the application.")
-
-	// example description
-	fmt.Println("\nExample:")
-	fmt.Println("	govault add -key email -value myemailpassword")
-	fmt.Println("	govault get -key email")
-	fmt.Println("	govault purge")
 }
