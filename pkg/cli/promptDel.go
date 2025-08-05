fb57cee00911c0afef30070f375f75da7f925d15
keith <keithneo00@gmail.com>
2025-08-05 17:57:05 +0800

    feat(cli/vars): Added a constand variable for Stdin reader instead of creating a new one for every file.
    refactor(cli/): Used the new constant reader

--- pkg/cli/promptDel.go
+++ pkg/cli/promptDel.go
@@ -1,18 +1,14 @@
 package cli
 
 import (
-	"bufio"
-	"os"
 	"strings"
 )
 
 func PromptDel() bool {
-	reader := bufio.NewReader(os.Stdin)
-
 	for {
 		Error("Are you sure you want to delete this secret?\n")
 		Warn("This action is irreversible and the secret cannot be recoved. (yes/no): ")
-		confirm, _ := reader.ReadString('\n')
+		confirm, _ := Reader.ReadString('\n')
 
 		confirm = strings.TrimSpace(confirm)
 
--- pkg/cli/purge.go
+++ pkg/cli/purge.go
@@ -1,19 +1,15 @@
 package cli
 
 import (
-	"bufio"
-	"os"
 	"strings"
 )
 
 func PromptPurge() bool {
-	reader := bufio.NewReader(os.Stdin)
-
 	for {
 		Error("WARNING: This action will permanently delete all stored secrets and reset the vault.\n")
 		Error("This operation cannot be undone.\n\n")
 		Warn("Are you sure you want to continue? (yes/no): ")
-		confirm, _ := reader.ReadString('\n')
+		confirm, _ := Reader.ReadString('\n')
 
 		// format input
 		confirm = strings.TrimSpace(confirm)
